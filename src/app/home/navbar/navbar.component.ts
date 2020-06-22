3b6569fe4426ab7c0b6ce790265562e6e3b88d08
hashem-buzer <hbuzer98@gmail.com>
2020-06-22 22:31:09 +0100

    v8

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -10,19 +10,22 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.changeTheme();
+    this.checkTheme();
   }
 
-  // checkTheme() {
-  //   if (localStorage.getItem('theme') === 'light') {
-  //     this.changeTheme('light');
-  //     localStorage.setItem('theme', 'dark');
-  //   } else if (localStorage.getItem('theme') === 'dark') {
-  //     localStorage.setItem('theme', 'light');
-  //   } else {
-  //     localStorage.setItem('theme', 'light');
-  //   }
-  // }
+  checkTheme() {
+    if (localStorage.getItem('theme') === 'light') {
+      // this.changeTheme('light');
+      localStorage.setItem('theme', 'dark');
+      this.changeTheme();
+    } else if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      this.changeTheme();
+    } else {
+      localStorage.setItem('theme', 'light');
+      this.changeTheme();
+    }
+  }
 
   changeTheme() {
     if (localStorage.getItem('theme') === 'light') {
@@ -62,7 +65,6 @@ export class NavbarComponent implements OnInit {
       $('#all').css('color', 'white');
       localStorage.setItem('theme', 'light');
     } else {
-      localStorage.setItem('theme', 'light');
       $('#theme').html(`
                 <svg
             class="bi bi-moon"
@@ -85,43 +87,5 @@ export class NavbarComponent implements OnInit {
       );
       $('#all').css('color', 'black');
     }
-
-    // if (theme === 'light') {
-    //   $('#theme').html(`
-    //             <svg
-    //         class="bi bi-moon"
-    //         width="1em"
-    //         height="1em"
-    //         viewBox="0 0 16 16"
-    //         fill="currentColor"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //       >
-    //         <path
-    //           fill-rule="evenodd"
-    //           d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"
-    //         />
-    //       </svg>
-    //   `);
-
-    //   $('#all').css(
-    //     'background',
-    //     'linear-gradient(to right, #afb8af, #ffffff)'
-    //   );
-    //   $('#all').css('color', 'black');
-
-    //   // this.checkTheme();
-    // } else if (theme === 'dark') {
-    //   $('#theme').html(`
-    //   <svg class="bi bi-brightness-high" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    //   <path fill-rule="evenodd" d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-    //   </svg>
-    //   `);
-
-    //   $('#all').css(
-    //     'background',
-    //     'linear-gradient(to bottom, #232526, #414345)'
-    //   );
-    //   $('#all').css('color', 'white');
-    // }
   }
 }
