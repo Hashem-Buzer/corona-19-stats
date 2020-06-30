94a5192457c5fd8273a158531f9d5c14e6ce1d3f
Malik Altawati <Malik-Altawati@gmail.com>
2020-06-30 13:34:48 -0700

    change language listner added to service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+/******/
+import { Subject } from 'rxjs';
+/******/
+
+
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  constructor(private http: HttpClient) {}
+
+  /******/
+  private listner = new Subject<any>();
+  reader$ = this.listner.asObservable();
+  language = "ar"
+  /******/
+
+  constructor(private http: HttpClient) { }
+
+  changeLang() {
+    if (this.language == "ar") this.language = "en"
+    else this.language = "ar"
+
+    this.listner.next(this.language)
+  }
 
   getData(country) {
     return this.http.get(`https://api.covid19api.com/total/country/${country}`);
